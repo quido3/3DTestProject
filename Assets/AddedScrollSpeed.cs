5dd10042fa824c1be56498d78716f6c5510bcbde
noppander <eemeli.jokipera@gmail.com>
2014-08-12 18:10:52 +0300

    Parallax Scroll Made
    Made the parallax and started to make the map

---
+++ Assets/AddedScrollSpeed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddedScrollSpeed : MonoBehaviour {
+	
+	public float speed;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		ParallaxController.addedScroll = speed;
+		
+	}
+}
---
+++ Assets/scripts/ParallaxController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxController : MonoBehaviour {
+
+	public float scrollSpeed;
+	public static float addedScroll;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		Vector3 pos = this.transform.position;
+		
+		pos.x -= scrollSpeed + addedScroll;
+		
+		this.transform.position = pos;
+		
+		print (gameObject.transform.localPosition.x + "Tämän edellä näkyvän pitäisi olla pienempi kuin tämän: " + this.transform.collider2D.bounds.size.x * -1);
+		
+		//print (this.gameObject.tag + this.transform.localPosition);
+		
+		if (gameObject.transform.localPosition.x < this.transform.collider2D.bounds.size.x * -1) {
+			gameObject.transform.localPosition = new Vector3 (this.transform.collider2D.bounds.size.x, this.transform.localPosition.y, 0 );
+			print ("Nyt mun taytyy liikkuu");
+			
+		}
+		
+		
+	}
+}
