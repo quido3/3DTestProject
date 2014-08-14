e01073059e9b2ebce3090d610be4216307fed66c
quido3 <koippalainen@hotmail.com>
2014-08-14 11:55:04 +0300

    Fixes
    Button to menu and structural fixes

---
+++ Assets/scripts/Universal/LoadSceneBtn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneBtn : MonoBehaviour
+{
+
+    public bool loadMainMenu = false;
+    public bool loadPlayScene = false;
+
+    void OnMouseDown()
+    {
+        if (loadMainMenu)
+        {
+            Application.LoadLevel(0);
+        }
+        if (loadPlayScene)
+        {
+            Application.LoadLevel(1);
+        }
+    }
+
+}
