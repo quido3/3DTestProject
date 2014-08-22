18c1fb0632cee0d4333448861f5de992c5568ec9
quido3 <koippalainen@hotmail.com>
2014-08-22 12:17:12 +0300

    Began rest of the game
    Began making the basic stucture of the menus. Edited woth scenes a
    little.

--- Assets/SceneScript.cs
+++ Assets/SceneScript.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class SceneScript : MonoBehaviour
 {
 
+    private int currentLevel = 0;
+    private int currectPoints = 0;
+
     // Use this for initialization
     void Start()
     {
-
+        currentLevel = PlayerPrefs.GetInt(SS.Level);
+        currectPoints = PlayerPrefs.GetInt(SS.Points);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Application.targetFrameRate = 60;
+        Application.targetFrameRate = 60;
     }
 }
